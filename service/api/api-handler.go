c120b040d409fdb7268b9f869a27d515370a83e6
Attilio Vanacore <attiliovanacore01@gmail.com>
2024-01-05 19:21:38 +0100

    remvoed wrappers in api-handlers, added structures and implemented the login endpoint

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -11,56 +11,56 @@ func (rt *_router) Handler() http.Handler {
 	rt.router.GET("/context", rt.wrap(rt.getContextReply))
 
 
-	rt.router.POST("/session", rt.wrap(rt.createUser))
+	rt.router.POST("/session", rt.getAuthToken)
 
-	rt.router.GET("/users", rt.wrap(rt.getUsersList))
+	rt.router.GET("/users", rt.getUsersList)
 
-	rt.router.GET("/users/:userId", rt.wrap(rt.getUserProfile))
-	rt.router.PUT("/users/:userId", rt.wrap(rt.updateUserProfile))
-	rt.router.DELETE("/users/:userId", rt.wrap(rt.deleteUserProfile))
+	rt.router.GET("/users/:userId", rt.getUserProfile)
+	rt.router.PUT("/users/:userId", rt.updateUserProfile)
+	rt.router.DELETE("/users/:userId", rt.deleteUserProfile)
 
-	rt.router.GET("/users/:userId/posts", rt.wrap(rt.getUserPosts))
-	rt.router.POST("/users/:userId/posts", rt.wrap(rt.createPost))
+	rt.router.GET("/users/:userId/posts", rt.getUserPosts)
+	rt.router.POST("/users/:userId/posts", rt.createPost)
 
-	rt.router.GET("/users/:userId/posts/postId", rt.wrap(rt.getPost))
-	rt.router.PUT("/users/:userId/posts/postId", rt.wrap(rt.editPost))
-	rt.router.DELETE("/users/:userId/posts/postId", rt.wrap(rt.deletePost))
+	rt.router.GET("/users/:userId/posts/postId", rt.getPost)
+	rt.router.PUT("/users/:userId/posts/postId", rt.editPost)
+	rt.router.DELETE("/users/:userId/posts/postId", rt.deletePost)
 
-	rt.router.GET("/users/:userId/posts/postId/likes", rt.wrap(rt.getPostLikes))
+	rt.router.GET("/users/:userId/posts/postId/likes", rt.getPostLikes)
 
-	rt.router.PUT("/users/:userId/posts/:postId/likes/:likeId", rt.wrap(rt.editPost))
-	rt.router.DELETE("/users/:userId/posts/:postId/likes/:likeId", rt.wrap(rt.deletePost))
+	rt.router.PUT("/users/:userId/posts/:postId/likes/:likeId", rt.editPost)
+	rt.router.DELETE("/users/:userId/posts/:postId/likes/:likeId", rt.deletePost)
 
-	rt.router.GET("/users/:userId/posts/postId/comments", rt.wrap(rt.getPostComments))
-	rt.router.POST("/users/:userId/posts/postId/comments", rt.wrap(rt.createComment))
+	rt.router.GET("/users/:userId/posts/postId/comments", rt.getPostComments)
+	rt.router.POST("/users/:userId/posts/postId/comments", rt.createComment)
 
-	rt.router.GET("/users/:userId/posts/postId/comments/:commentId", rt.wrap(rt.getComment))
-	rt.router.PUT("/users/:userId/posts/postId/comments/commentId",rt.wrap(rt.editComment))
-	rt.router.DELETE("/users/:userId/posts/postId/comments/:commentId",rt.wrap(rt.deleteComment))
+	rt.router.GET("/users/:userId/posts/postId/comments/:commentId", rt.getComment)
+	rt.router.PUT("/users/:userId/posts/postId/comments/commentId",rt.editComment)
+	rt.router.DELETE("/users/:userId/posts/postId/comments/:commentId",rt.deleteComment)
 
-	rt.router.GET("/users/:userId/posts/postId/comments/:commentId/likes", rt.wrap(rt.getCommentLikes))
+	rt.router.GET("/users/:userId/posts/postId/comments/:commentId/likes", rt.getCommentLikes)
 
-	rt.router.PUT("/users/:userId/posts/postId/comments/:commentId/likes/likeId", rt.wrap(rt.likeComment))
-	rt.router.DELETE("/users/:userId/posts/postId/comments/:commentId/likes/likeId", rt.wrap(rt.unlikeComment))
+	rt.router.PUT("/users/:userId/posts/postId/comments/:commentId/likes/likeId", rt.likeComment)
+	rt.router.DELETE("/users/:userId/posts/postId/comments/:commentId/likes/likeId", rt.unlikeComment)
 
-	rt.router.GET("/users/:userId/follower", rt.wrap(rt.getFollowersList))
+	rt.router.GET("/users/:userId/follower", rt.getFollowersList)
 	
-	rt.router.GET("/user/:userId/following", rt.wrap(rt.getFollowingsList))
+	rt.router.GET("/user/:userId/following", rt.getFollowingsList)
 	
-	rt.router.PUT("/users/:userId/following/:followingId", rt.wrap(rt.followUser))
-	rt.router.DELETE("/users/:userId/following/followingId", rt.wrap(rt.unfollowUser))
+	rt.router.PUT("/users/:userId/following/:followingId", rt.followUser)
+	rt.router.DELETE("/users/:userId/following/followingId", rt.unfollowUser)
 
-	rt.router.GET("/users/:userId/banned", rt.wrap(rt.getUserBanList))
+	rt.router.GET("/users/:userId/banned", rt.getUserBanList)
 
-	rt.router.PUT("/users/:userId/banned/:bannedId", rt.wrap(rt.banUser))
-	rt.router.DELETE("/users/:userId/banned/:bannedId", rt.wrap(rt.unbanUser))
+	rt.router.PUT("/users/:userId/banned/:bannedId", rt.banUser)
+	rt.router.DELETE("/users/:userId/banned/:bannedId", rt.unbanUser)
 
-	rt.router.POST("/users/:userId/photos", rt.wrap(rt.uploadPhoto))
+	rt.router.POST("/users/:userId/photos", rt.uploadPhoto)
 
-	rt.router.GET("/users/:userId/photos/photoId", rt.wrap(rt.getPhoto))
-	rt.router.DELETE("/users/:userId/photos/photoId", rt.wrap(rt.deletePhoto))
+	rt.router.GET("/users/:userId/photos/photoId", rt.getPhoto)
+	rt.router.DELETE("/users/:userId/photos/photoId", rt.deletePhoto)
 
-	rt.router.GET("/users/:userId/feed", rt.wrap(rt.getFeed))
+	rt.router.GET("/users/:userId/feed", rt.getFeed)
 
 	// Special routes
 	rt.router.GET("/liveness", rt.liveness)
---
+++ service/api/auth.go
@@ -0,0 +1,45 @@
+package api
+
+import (
+    "encoding/json"
+    "net/http"
+    "github.com/julienschmidt/httprouter"
+)
+
+// getToken is the handler for POST /session
+// Parse the request body which should contain a username,
+// and return the userId if the username is valid.
+// If user is not present in the database, it will be created
+// and the userId will be returned.
+func (rt *_router) getAuthToken(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+    // Parse and decode the request body into a string
+    var username string
+    err := json.NewDecoder(r.Body).Decode(&username)
+    if err != nil {
+        // If there is something wrong with the request body, return a 400 status
+        w.WriteHeader(http.StatusBadRequest)
+        return
+    }
+
+    // Get the user from the database
+    user, err := rt.db.getUserFromDB(username)
+
+    // If the user doesn't exist, create a new user
+    if err != nil {
+        user, err = rt.db.createUserInDB(username)
+        if err != nil {
+            // If there was an error creating the user, return a 500 status
+            w.WriteHeader(http.StatusInternalServerError)
+            return
+        }
+        // If a new user was created, return a 201 status
+        w.WriteHeader(http.StatusCreated)
+    } else {
+        // If the user already existed, return a 200 status
+        w.WriteHeader(http.StatusOK)
+    }
+
+    // Return the user's ID in the response body
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(ResourceID{Value: user.UserID})
+}
