2184e403deed2fcf4d99c3746fa5cd0b1cca94d0
Attilio Vanacore <attiliovanacore01@gmail.com>
2024-02-10 20:49:37 +0100

    working on the app.vue file

--- webui/src/App.vue
+++ webui/src/App.vue
@@ -1,40 +1,87 @@
 <template>
 	<div>
-		<!-- Check if the user is logged in -->
-		<template v-if="isLoggedIn">
-			<!-- The user is logged in, show the stream page -->
-			<router-view></router-view>
-
-			<!-- Bottom bar with buttons -->
-			<div class="bottom-bar">
-				<router-link to="/home">Home</router-link>
-				<router-link to="/profile">Profile</router-link>
-				<router-link to="/search">Search</router-link>
-			</div>
-		</template>
-
-		<!-- The user is not logged in, show the login page -->
-		<router-view v-else></router-view>
+	  <!-- The router-view will automatically render the correct component -->
+	  <router-view></router-view>
+  
+	  <!-- Sidebar with links -->
+	  <div class="sidebar" v-if="showSidebar">
+		<div class="title-container">
+		  <h2 class="sidebar-title">WASAPhoto</h2>
+		</div>
+		
+		<ul class="sidebar-links">
+		  <li><router-link to="/home">Home</router-link></li>
+		  <li><router-link to="/profile">Profile</router-link></li>
+		  <li><router-link to="/search">Search</router-link></li>
+		</ul>
+	  </div>
 	</div>
-</template>
-
+  </template>
+  
 <script setup>
-import { ref } from 'vue';
-import { RouterLink, RouterView } from 'vue-router';
+import { ref, watchEffect } from 'vue';
+import { useRoute } from 'vue-router';
 
-// Check if the user is logged in;
-const isLoggedIn = ref(false);
-if (sessionStorage.getItem('token')) {
-	isLoggedIn.value = true;
-}
+// Get the current route
+const route = useRoute();
+
+// Determine whether to show the sidebar
+const showSidebar = ref(false);
+
+watchEffect(() => {
+	// Show the sidebar if the current route is /home, /profile, or /search
+	showSidebar.value = ['/home', '/profile', '/search'].includes(route.path);
+});
 </script>
-
+  
 <style>
-.bottom-bar {
+.sidebar {
 	position: fixed;
-	bottom: 0;
-	width: 100%;
-	display: flex;
-	justify-content: space-around;
+	left: 0;
+	width: 200px;
+	height: 100%;
+	background-color: #15202b;
+	padding: 0;
+	color: #fff;
+}
+
+.title-container {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  border-bottom: 1px solid #393864;
+}
+
+.sidebar-title {
+  align-self: flex-start; /* Add this line */
+  margin-top: 0px;
+  margin-bottom: 20px;
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+}
+
+.sidebar-links {
+	list-style-type: none;
+	padding: 0;
+}
+
+.sidebar-links li {
+	margin-bottom: 0px;
+}
+
+.sidebar-links li a {
+	color: #fff;
+	text-decoration: none;
+	font-size: 18px;
+	font-weight: bold;
+	transition: background-color 0.3s ease;
+	padding: 10px;
+	border-radius: 15px;
+	display: inline-block;
+}
+
+.sidebar-links li a:hover {
+	background-color: rgba(255, 255, 255, 0.1);
 }
 </style>
