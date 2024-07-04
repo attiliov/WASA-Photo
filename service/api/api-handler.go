171d5632c6fd40bbeab30f96e46d65dfe9bd23f1
Attilio Vanacore <attiliovanacore01@gmail.com>
2024-07-04 13:23:26 +0200

    comments

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -12,7 +12,7 @@ func (rt *_router) Handler() http.Handler {
 
 	rt.router.POST("/session", rt.getAuthToken) // TESTED, TESTED ON FRONTEND TODO: add last seen update and chech why dates are not working properly in db
 
-	rt.router.GET("/users", rt.searchUser) // TESTED
+	rt.router.GET("/users", rt.searchUser) // TESTED on frontend
 
 	rt.router.GET("/users/:userId", rt.getUserProfile)       // TESTED, on frontend
 	rt.router.PUT("/users/:userId", rt.updateUserProfile)    // TESTED, ON FRONTEND
@@ -42,24 +42,24 @@ func (rt *_router) Handler() http.Handler {
 	rt.router.PUT("/users/:userId/posts/:postId/comments/:commentId/likes/:likeId", rt.likeComment)      // TESTED, ON FRONTEND
 	rt.router.DELETE("/users/:userId/posts/:postId/comments/:commentId/likes/:likeId", rt.unlikeComment) // TESTED, ON FRONTEND
 
-	rt.router.GET("/users/:userId/followers", rt.getFollowersList) // TESTED
+	rt.router.GET("/users/:userId/followers", rt.getFollowersList) // TESTED on frontend
 
-	rt.router.GET("/users/:userId/following", rt.getFollowingsList) // TESTED
+	rt.router.GET("/users/:userId/following", rt.getFollowingsList) // TESTED on frontend
 
-	rt.router.PUT("/users/:userId/following/:followingId", rt.followUser)      // TESTED
-	rt.router.DELETE("/users/:userId/following/:followingId", rt.unfollowUser) // TESTED
+	rt.router.PUT("/users/:userId/following/:followingId", rt.followUser)      // TESTED on frontend
+	rt.router.DELETE("/users/:userId/following/:followingId", rt.unfollowUser) // TESTED on frontend
 
 	rt.router.GET("/users/:userId/banned", rt.getUserBanList) // TESTED
 
-	rt.router.PUT("/users/:userId/banned/:bannedId", rt.banUser)      // TESTED
-	rt.router.DELETE("/users/:userId/banned/:bannedId", rt.unbanUser) // TESTED
+	rt.router.PUT("/users/:userId/banned/:bannedId", rt.banUser)      // TESTED on frontend
+	rt.router.DELETE("/users/:userId/banned/:bannedId", rt.unbanUser) // TESTED on frontend
 
 	rt.router.POST("/users/:userId/photos", rt.savePhoto) // TESTED on frontend
 
 	rt.router.GET("/users/:userId/photos/:photoId", rt.getPhoto)       // TESTED on frontend
 	rt.router.DELETE("/users/:userId/photos/:photoId", rt.deletePhoto) // TESTED on frontend
 
-	rt.router.GET("/users/:userId/feed", rt.getFeed) // TESTED
+	rt.router.GET("/users/:userId/feed", rt.getFeed) // TESTED on frontend
 
 	// Special routes
 	rt.router.GET("/liveness", rt.liveness)
